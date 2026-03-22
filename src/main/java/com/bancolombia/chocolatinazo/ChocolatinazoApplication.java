9feaf52218a6f12399acc5276769490b935ed2ab
Mateo <teocarias21@gmail.com>
2026-03-21 23:17:03 -0500

    Commit inicial

---
+++ src/main/java/com/bancolombia/chocolatinazo/ChocolatinazoApplication.java
@@ -0,0 +1,13 @@
+package com.bancolombia.chocolatinazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocolatinazoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChocolatinazoApplication.class, args);
+    }
+
+}
